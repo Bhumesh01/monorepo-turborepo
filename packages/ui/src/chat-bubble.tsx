40faaa71fd895a78c984f6d8866ec9d570f0a7d3
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-13 16:41:14 +0530

    backends.json

---
+++ packages/ui/src/chat-bubble.tsx
@@ -0,0 +1,7 @@
+export function ChatBubble(){
+    return(
+        <div style={{backgroundColor: "#002f5a", width: "fit-content", padding: "15px", borderRadius: "15px"}}>
+            Message
+        </div>
+    )
+}
